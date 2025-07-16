a77051b8f8bc1bf8a626eddde4c1a910f066d8f1
Stephani Bustos <s.bustos@seeri.co>
2025-07-15 20:15:10 -0500

    Add To Cart

--- tests/add-Products.spec.ts
+++ tests/add-Products.spec.ts
@@ -2,12 +2,17 @@ import { test, expect } from "@playwright/test";
 import { LoginPage } from "./page-object/loginPage";
 import { InventoryPage } from "./page-object/inventoryPage";
 
-test("login", async ({ page }) => {
-  await page.goto("https://www.saucedemo.com/v1/inventory.html");
+test("add product to cart", async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/v1/index.html");
+
   const authentication = new LoginPage(page);
   const inventoryPage = new InventoryPage(page);
+
+  await authentication.enterUserName();
+
+  await authentication.enterPassword();
+
   await authentication.clickLoginButton();
   await inventoryPage.validateShoppingCart();
-  await page.getByRole("button", { name: "Add to cart" }).first().click();
-  const loginButton = page.locator("input[id='login-button']");
+  await inventoryPage.addProductsToCart();
 });
